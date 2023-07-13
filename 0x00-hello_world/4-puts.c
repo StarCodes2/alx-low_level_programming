7d422318c92bf88faef8d59866fc2706cd7c7605
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-13 14:58:18 -0700

    Uses the puts() function, fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+/* Prints using the puts() fnction */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
