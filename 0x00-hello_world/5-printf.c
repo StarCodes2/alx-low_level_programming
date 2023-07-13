38b26a82dc22aed45d7f450213e68f8458249393
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-13 15:17:34 -0700

    usung printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints with printf
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
