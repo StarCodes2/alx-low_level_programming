8fefa5bb77265455e27f5e6a42f2997416314600
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-14 15:07:05 -0700

    alphabelt in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabelts in reverse
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	char i ='z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
