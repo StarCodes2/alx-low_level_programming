dff5590e4cd12ffd2e3fc5a289a1d3b9b9ee6276
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-18 15:06:22 -0700

    Prints a-z 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,12 @@
 
 void print_alphabet_x10(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+	char a;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (char a = 'a'; a <= 'z'; a++)
+		for (a = 'a'; a <= 'z'; a++)
 		{
 			_putchar(a);
 		}
