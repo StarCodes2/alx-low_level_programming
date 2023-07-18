98f44a19807ac093a51522330a3d07dd1c28fb98
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-18 16:59:30 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the times table up to 9
+ * starting from zero
+ */
+
+void times_table(void)
+{
+	int a, b, mul;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			mul = a * b;
+
+			if (mul > 9)
+			{
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+			}
+			else
+			{
+				_putchar(mul + '0');
+			}
+
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
