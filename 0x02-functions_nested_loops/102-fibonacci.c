50c10c487f19de50612c74d400aaeba0016d9cb4
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-18 20:11:34 -0700

    50 numbers from the fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	int a = 1, b = 2, c, i;
+
+	printf("%d, %d", a, b);
+
+	for (i = 0: i <= 48; i++)
+	{
+		c = a + b;
+		printf(", %d", c);
+		a = b;
+		b = c;
+	}
+
+	printf("\n");
+
+	return (0);
+}
