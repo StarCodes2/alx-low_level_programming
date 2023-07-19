9aae461ddd36de12a2f604b9b407d4934bad2605
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-19 15:23:44 -0700

    test the positive_or_negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Tests the postitive_or_negative function
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	postitive_or_negative(i);
+
+	return (0);
+}
