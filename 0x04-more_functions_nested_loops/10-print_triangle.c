1577a490ad71cd3fcb2c2820ee6fe0b7ebccf99b
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-20 14:38:17 -0700

    To print a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print a triangle of a particular size
+ * @size: value determines the size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, a, b;
+
+	if (size > 0)
+	{
+		for (i = size; i > 0; i--)
+		{
+			for (a = i - 1; a > 0; a--)
+			{
+				_putchar(' ');
+			}
+
+			for (b = i - 1; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
