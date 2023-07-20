8600efd31f4d9499bae0423a08ff4203b541a8a5
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-20 10:26:04 -0700

    To add two integrs

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - add two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: returns the result of the addition
+ */
+
+int mul(int a, int b)
+{
+	return (a + b);
+}
