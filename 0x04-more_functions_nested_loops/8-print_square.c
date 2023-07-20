3b6a604928512908ef7531711f48b213860d0ff7
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-20 12:42:56 -0700

    To print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square using the hash sign
+ * and a number to determine its size
+ * @size: number that determnes the size of the square
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
