9093ab72324796322795686c924a31b0ada7962b
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-20 18:53:26 -0700

    To print integer of up to 4 digits with _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -16,10 +16,10 @@ void print_number(int n)
 		_putchar('-');
 	}
 
-	if ((n / 10) > 0)
+	if ((n2 / 10) > 0)
 	{
-		print_number(n / 10);
+		print_number(n2 / 10);
 	}
 
-	_putchar((n % 10) + '0');
+	_putchar((n2 % 10) + '0');
 }
