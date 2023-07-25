bd053c5fd09f34d40aa6974e1fe1d7ae0a8e5c66
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-25 07:37:14 -0700

    To reverse an array\'s of characters

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,26 @@
 
 void rev_string(char *s)
 {
-	int i = 0, count = 0, s2[];
+	int i = 0, count = 0;
 
 	while (*(s + i) != '\0')
 	{
-		s2[i] = s[i];
 		i++;
 	}
 
 	count = i - 1;
 	i = 0;
+	s2[count];
 
-	for (count; count >= 0; count--)
+	while (i <= count)
+	{
+		s2[i] = s[i];
+		i++;
+	}
+
+	i = 0;
+
+	for (count = count; count >= 0; count--)
 	{
 		*(s + i) = s2[count];
 		i++;
