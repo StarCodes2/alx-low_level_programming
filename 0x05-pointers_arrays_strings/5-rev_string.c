e5ec8a8a7b8140390431dc2b177dbd0eebebaba3
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-25 01:29:35 -0700

    To print an array of chars in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - print the content of an array of characters in reverse
+ * @s: contains the string
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	for (i -= 1; i >= 0; i--)
+	{
+		putchar(*(s + i));
+	}
+
+	putchar('\n');
+}
