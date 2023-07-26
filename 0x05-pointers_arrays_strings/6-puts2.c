cae4a5602af07e6fcd4c5c1d9f78e89d9b39b363
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-26 01:09:10 -0700

    edited

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,8 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0' || str[i - 1] != '\0'; i += 2)
 	{
-		if (str[i - 1] == '\\' || str[i] == '\\')
-		{
-			break;
-		}
 		_putchar(str[i]);
 	}
 
