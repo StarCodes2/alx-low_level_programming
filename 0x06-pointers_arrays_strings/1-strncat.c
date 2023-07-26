013b2811e192f5c625376bc8a3d336faedfb403d
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-26 13:23:27 -0700

    To add n characters of a string to another string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - Concat the n characters of the second strings to the first
+ * @dest: pointer to the first character of the first string
+ * @src: pointer to the first character of the second string
+ * @n: contains the number of characters of the second string
+ * to add to the first string
+ *
+ * Return: returns a pointer to the first character of the new string in dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index, index2 = 0, len = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+
+	for (index = len; index2 < n || src[index2] != '\0'; index++)
+	{
+		dest[index] = src[index2];
+		index2++;
+	}
+
+	return (dest);
+}
