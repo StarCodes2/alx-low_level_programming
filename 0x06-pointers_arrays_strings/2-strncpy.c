c9fe3f315fdabe913a1973866c0854998e205278
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-27 00:24:47 -0700

    To copy a string of n length from one memory location to another

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,12 +15,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
+		if (src[i] == '\0')
+			break;
+
 		dest[i] = src[i];
 	}
 
-	for (; i < n; i++)
+	for (i = i; i < n; i++)
 	{
 		dest[i] = '\0';
 	}
