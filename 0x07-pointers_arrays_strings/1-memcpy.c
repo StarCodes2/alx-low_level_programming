2f6808c2d15efba50c0bfb7b6677d8dcfca2d42e
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-31 13:22:17 -0700

    To copy the content of one memory area to another

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy's characters from memory location src to
+ * dest
+ * @dest: points to the memory location to copy the characters to
+ * @src: points the memory location where the characters to be copied are
+ * @n: contains the number of characters to be copied
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
