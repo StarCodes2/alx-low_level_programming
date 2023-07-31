810bf3cda3d98fe9688761da29af62b77f26f7f4
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-07-31 14:10:18 -0700

    To copy the content of one memory area to another

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,6 +6,8 @@
  * @dest: points to the memory location to copy the characters to
  * @src: points the memory location where the characters to be copied are
  * @n: contains the number of characters to be copied
+ *
+ * Return: returns a pointer to the destination location
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
