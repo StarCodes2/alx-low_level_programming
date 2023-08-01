ae913b2a7530ac8db7b674fdbf539774bc842714
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-01 07:57:33 -0700

    To locate a substring in a string if it exist

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - Searches for a substring in a string
+ * @haystack: Points to the string
+ * @needle: points to the substring
+ *
+ * Return: returns a pointer to the beginning of the substring in the
+ * string if found or NULL if not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i;
+
+	while (*haystack)
+	{
+		if (haystack[i] == needle[i])
+		{
+			for (i = 0; needle[i] != '\0'; i++)
+			{
+				if (haystack[i] != needle[i])
+					break;
+				else if (needle[i + 1] == '\0')
+					return (haystack);
+			}
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
