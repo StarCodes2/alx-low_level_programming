175f8f5843b9b22c0bdaaa146e2acf55520722be
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-03 10:06:18 -0700

    To get the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _sqrt - Checks for values that when when multiplied by itself equals to a
+ * given integer
+ * @n: contains the integer
+ * @sqr: contains the value to be multiplied by itself
+ *
+ * Return: returns the value in sqr if when multiplied by itself it produce
+ * the integer or -1 if no such value is found
+ */
+
+int _sqrt(int n, sqr)
+{
+	int sqr2 = sqr;
+
+	sqr2 *= sqr;
+
+	if (sqr2 == n)
+		return (sqr);
+	else if (sqr == n / 2)
+		return (-1);
+
+	return (_sqrt(n, sqr++));
+}
+
+/**
+ * _sqrt_recursion - Fknd the square root of an integer
+ * @n: contains the integer
+ *
+ * Return: returns the square root or -1 if the integer has no natural root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 1)
+		return (1);
+
+	return (_sqrt(n, 0));
+}
