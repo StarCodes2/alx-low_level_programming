2394959711e9eb0474ecb0ca08b997ffccae681f
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-04 18:09:17 -0700

    To add all numbers passed as argument

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the sum of numbers passedd as arguments to the program
+ * @argc: contain the number of arguments
+ * @argv: array of strings
+ *
+ * Return: returns 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum = sum + atoi(argv[i]);
+	}
+
+	return (0);
+}
