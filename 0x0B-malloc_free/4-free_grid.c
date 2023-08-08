f8618b194fb4a87713a6a50e71017eced413d8a9
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-08 12:10:47 -0700

    To free the memory spaces allocated to a 2D array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - free the memory spaces allocated for a 2D array
+ * @grid: points to the array
+ * @height: contains the number row in the grid or array
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
