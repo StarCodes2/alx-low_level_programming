e695d24722e6f863539c248636e335ded7ce8ffb
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-08 07:11:37 -0700

    To concat two strings and save the result in a newly allocated memory space

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - concat two strings and result in a newly allocated
+ * memory space
+ * @s1: points to the first string
+ * @s2: points to the second string
+ *
+ * Return: returns a pointer to the allocated memory and NULL if it fails
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int j, i = 0, size = 0;
+	char *s3;
+
+	while (s1[i])
+	{
+		size++;
+		i++;
+	}
+
+	i = 0;
+
+	while (s2[i])
+	{
+		size++;
+		i++;
+	}
+
+	s3 = malloc(sizeof(char) * size + 1);
+
+	if (s3 == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		s3[i] = s1[i];
+
+	for (j = 0; s2[j]; j++)
+	{
+		s3[i] = s2[j];
+		i++;
+	}
+	s3[i] = '\0';
+
+	return (s3);
+}
