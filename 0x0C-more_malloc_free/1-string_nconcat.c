556d26442e3b82438eebd162fbc7e40920a8ac8e
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-10 09:24:24 -0700

    To concat n characters of one string to another

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,10 +17,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s3;
 
 	if (s1 == NULL)
-		*s1 = "";
+		s1 = "";
 
 	if (s2 == NULL)
-		*s2 = "";
+		s2 = "";
 
 	for (index = 0; s1[index]; index++)
 		len1++;
