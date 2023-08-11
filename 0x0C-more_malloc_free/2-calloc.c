8a2e6768b0184df4aa05360bb6e242b583e8dc73
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-10 20:06:50 -0700

    To allpcate memory and initialize it to zero

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array and initialize it
+ * @nmemb: contains the number of elements the area would contain
+ * @size: contains the size of each elements in the array
+ *
+ * Return: returns a pointer the allocated memory, NULL if it fails
+ * NULL if nmemb or size equals zero
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, m_size;
+	void *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	m_size = nmemb * size;
+
+	arr = malloc(m_size);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < m_size; i++)
+	{
+		arr[i] = '\0';
+	}
+
+	return (arr);
+}
