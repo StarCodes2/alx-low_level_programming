d6f0a8664edc90967dd7d8379c8f4db9640d8bf4
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-11 01:04:18 -0700

    To reallocate a memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,4 +12,41 @@
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{}
+{
+	void *new_ptr;
+	char *old_char, *new_char;
+	unsigned int i;
+
+	if (old_size == new_size)
+	{
+		return (ptr);
+	}
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+
+		if (new_ptr == NULL)
+			return (NULL);
+	}
+	else if (old_size > 0 && new_size > 0)
+	{
+		new_ptr = malloc(new_size);
+
+		if (new_ptr == NULL)
+			return (NULL);
+
+		old_char = ptr;
+		new_char = new_ptr;
+
+		for (i = 0; i < old_size && i < new_size; i++)
+			new_char[i] = new_char[i];
+		free(ptr);
+	}
+
+	return (new_ptr);
+}
