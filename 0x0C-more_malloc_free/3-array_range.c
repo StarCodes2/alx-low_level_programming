d66d6abfce9be38a8ec080c7822db3e5f58c800e
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-10 21:06:31 -0700

    To create an array of integers that holds a range of numbers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,7 +1,31 @@
 #include <stdlib.h>
 #include "main.h"
 
-
+/**
+ * array_range - creates an array of integers that hold values from min to max
+ * @min: contains the first value
+ * @max: contains the last value
+ *
+ * Return: returns a pointer to the new array or NULL if min > max or fails
+ */
 
 int *array_range(int min, int max)
-{}
+{
+	int len, i;
+	int *arr;
+
+	if (min > max)
+		return (NULL);
+
+	len = max - min + 1;
+
+	arr = malloc(sizeof(int) * len);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		arr[i] = min++;
+
+	return (arr);
+}
