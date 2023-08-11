101c965e2aa8fb66088ffa936d579838ebd9f595
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-10 21:32:27 -0700

    To allpcate memory and initialize it to zero

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _realloc - reallocates a block of memory using malloc and free
+ * @ptr: points to the old memory block
+ * @old_size: contains the size of the old memory block in byte
+ * @new_size: contains the size of the new memory block in byte
+ *
+ * Return: return a pointer to the new memory block if success,
+ *	NULL if it fails or new_size equals 0 and ptr equal NULL
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{}
