9974da0c9efe9543157e3a52cd31d9abe08bdfc7
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-15 13:24:17 -0700

    To initialize a struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,19 +8,13 @@
  * @owner: Will point to the owner's name
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
-/**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
 
 #endif
