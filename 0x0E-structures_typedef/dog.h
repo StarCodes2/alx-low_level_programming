345a8d2fd8d3c9a7fec61396d1bd813e2752433e
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-15 12:37:58 -0700

    To define a new data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a structure dog with three members
+ * @name: Points to the dog's name
+ * @age: will hold the dog's age
+ * @owner: Will point to the owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+#endif
