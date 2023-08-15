fbee67c77e887c2087fc8bc2a76f30bbe9c72e7f
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-15 13:44:54 -0700

    To print the content of the elements of a struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints the content of the elements of a struct
+ * @d: Points to the struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+
+		if (d->age != 0)
+			printf("Age: %f\n", d->age);
+		else
+			printf("Age: (nil)\n");
+
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
