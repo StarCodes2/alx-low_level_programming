0dbc76103fc76c3bdecdb2e87ae135d2e7081afd
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-16 14:51:34 -0700

    To print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Calls a function using a pointer and pass it a string
+ * @name: points to a string
+ * @f: points to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
