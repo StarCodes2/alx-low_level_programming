cc607d5de960a60d544ccf23fbf1a7d137f626ee
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-16 21:36:23 -0700

    To search for an integer in an array using a compare function called with a pointer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Search through an array for an integer
+ * @array: points to the array
+ * @size: contains the array size
+ * @cmp: points to the compare function
+ *
+ * Return: return's the index where the integer is found in an array
+ * or -1 if it fails or none is found
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array || size < 1 || cmp)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
