d323486705958f5f62c5c2443cf671fc31e345b2
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-17 14:01:17 -0700

    To print all undefined arguments

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print the integer arguments
+ * @separator: points to the string that will separate the numbers
+ * @n: contains the number of undefined arguments
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list p;
+	unsigned int index;
+
+	if (n == 0)
+		return;
+
+	va_start(p, n);
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(p, int));
+		if (index < (n - 1) && separator != NULL)
+			printf("%s", separaror);
+	}
+	va_end(p);
+	printf("\n");
+}
