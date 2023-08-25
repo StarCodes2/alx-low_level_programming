708863a14534816f28455d148865d46b3bf584a3
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-25 15:51:19 -0700

    To add a new node to the end of the list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,72 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node_end - adds a new node at the beginning of a list
+ * @head: points to a pointer that points to a list
+ * @str: points to a string
+ *
+ * Return: returns an address to the first node in the list
+ */
+
+list_t *add_node_end(list_t **head, const char *str);
+{
+	char *s;
+	list_t *new = malloc(sizeof(list_t));
+	list_t *last;
+
+	if (new == NULL)
+		return (NULL);
+
+	s = strdup(str);
+
+	if (s == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->str = s;
+	new->len = len(s);
+	new->next = NULL;
+
+	last = get_last_node(*head);
+	last->next = new;
+
+	return (*head);
+}
+
+/**
+ * len - counts the length of a string
+ * @s: points to a string
+ *
+ * Return: returns the length of the string
+ */
+
+size_t len(char *s)
+{
+	int i;
+	size_t len = 0;
+
+	for (i = 0; s[i]; i++)
+		len++;
+
+	return (len);
+}
+
+/**
+ * get_last_node - gets the last node in a list
+ * @h: points to the first node in the list
+ *
+ * Return: returns the address of the last node
+ */
+
+list_t get_last_node(list_t *h)
+{
+	if (h->next)
+		node = get_last_node(h->next);
+	else
+		node = h;
+
+	return (node);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,5 +28,6 @@ list_t *add_node(list_t **head, const char *str);
 void print_str(char *s);
 void print_num(unsigned int len);
 size_t len(char *s);
+list_t get_last_node(list_t *h);
 
 #endif
