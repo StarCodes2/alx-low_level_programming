870313ec80c9bba472ab61dc1965fe284c88d885
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-29 12:57:59 -0700

    Comments added

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,8 +1,15 @@
 #include "lists.h"
 
+/**
+ * free_listint - frees the memory spaces allocated to a list
+ * @head: points to the first node in the list
+ */
 
 void free_listint(listint_t *head)
 {
+	if (head == NULL)
+		return (NULL);
+
 	if (head->next)
 		free_listint(head->next);
 
