a8c67d5ee29ad95b8f0380ca594e94d30ba7ad88
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-29 10:17:26 -0700

    Bug fixed: last nodes link not initialized to null

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,19 +10,20 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new, *temp;
+	listint_t *new, *last;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
+	new->next = NULL;
 
-	temp = get_last_node(*head);
-	if (temp == NULL)
+	last = get_last_node(*head);
+	if (last == NULL)
 		*head = new;
 	else
-		temp->next = new;
+		last->next = new;
 
 	return (new);
 }
