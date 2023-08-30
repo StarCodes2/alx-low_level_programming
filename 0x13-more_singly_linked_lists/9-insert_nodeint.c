1d8fe17fa926e01deccb68432ba7907912ec78ba
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-08-30 14:58:00 -0700

    To add a new node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *new;
 
-	if (*head == NULL)
+	if (*head == NULL && idx != 0)
 		return (NULL);
 
 	for (i = 0; *head; i++)
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = (*head)->next;
 	}
 
-	if (((idx - 1) > i) || *head == NULL)
+	if (((idx - 1) > i) && *head == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
