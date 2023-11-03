b6a0822532e2a3df67c9809459280ac2ce09de4c
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-11-03 13:01:48 -0700

    To create a new hash table of type hash_table_t

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a new hash table of type hash_table_create
+ * @size: contains the size of the hash table
+ *
+ * Return: a pointer to a new hash table on success or NULL if it fails
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *table;
+
+	if (size == 0)
+		return (NULL);
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
