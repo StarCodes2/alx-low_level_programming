01d07085d855697e0c40b7dd35ef60f188af809e
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-11-03 18:12:48 -0700

    To print all key-value pairs in the hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints all the key-value pairs in the hash table
+ * @ht: points to the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int p_check = 0;
+	hash_node_t *node;
+
+	if (ht != NULL)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i] != NULL)
+			{
+				node = ht->array[i];
+				if (p_check)
+					printf(", ");
+				printf("%s:%s", node->key, node->value);
+				p_check = 1;
+
+				while (node->next)
+				{
+					node = node->next;
+					printf(", %s:%s",
+						node->key, node->value);
+				}
+			}
+		}
+	}
+}
